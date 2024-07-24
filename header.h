cedb5d9567c5dda39380d8ad3e5aec05140d888e
Rar <166827921+rarcns@users.noreply.github.com>
2024-07-24 14:00:07 +0900

    Update header.h

--- header.h
+++ header.h
@@ -10,4 +10,6 @@ extern void Open_Website(std::string Link);
 extern void ask(std::string question, std::string &retur);
 extern void intask(const std::string& question, int& retur);
 extern void ucharask(const std::string& question, unsigned char& retur);
+extern void dwask(const std::string& question, DWORD& retur);
+extern void byteask(const std::string& question, BYTE& retur);
 #endif // HEADER_H
